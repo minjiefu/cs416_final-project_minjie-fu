9896360cfc9e4aec8c9ff4555fefc440ecb73438
MINJIE FU <melody.minjie.fu@gmail.com>
2022-07-15 10:15:26 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -91,7 +91,7 @@ d3.csv("data/revenues.csv").then(data => {
     
     rects.enter().append("text")
     
-    .attr("x", (d) => x(d.month)+x.bandwidth-8)
+    .attr("x", (d) => x(d.month)+20)
         .attr("y",  d => HEIGHT)
             .attr("height", 0)
                 .transition()
