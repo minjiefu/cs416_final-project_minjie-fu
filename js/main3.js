bcce831b0f9998d31865acfa93fd7f7790780ed6
MINJIE FU <melody.minjie.fu@gmail.com>
2022-07-15 23:14:08 -0600

    Add files via upload

--- js/main3.js
+++ js/main3.js
@@ -19,7 +19,7 @@ svg.append("text")
    .attr("text-anchor", "middle")
    .style("font-size", "25px")
    .style("font-weight", "bold")
-   .text("How old are the collected menus");
+   .text("How old are the collected menus?");
 
 
 // Y label
@@ -83,6 +83,8 @@ g.append("text")
  .attr("stroke-linecap", "round")
  .attr("stroke-width", 1.5)
  .attr("d", line)
+ .transition()
+ .duration(5000)
  
  
  
