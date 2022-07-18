2afa5efc20252ae4fd862557a7ab3919cdd7c8e7
MINJIE FU <melody.minjie.fu@gmail.com>
2022-07-18 02:31:18 -0600

    Add files via upload

--- js/main2.js
+++ js/main2.js
@@ -2,6 +2,7 @@ const MARGIN = { LEFT: 120, RIGHT: 10, TOP: 100, BOTTOM: 100 }
 const WIDTH = 800 - MARGIN.LEFT - MARGIN.RIGHT
 const HEIGHT = 800 - MARGIN.TOP - MARGIN.BOTTOM
 
+
 let flag = true
 
 const svg = d3.select("#chart-area").append("svg")
@@ -24,7 +25,7 @@ svg.append("text")
 // Y label
 const yLabel = g.append("text")
   .attr("class", "y axis-label")
-  .attr("x", - (HEIGHT / 2))
+  .attr("x", - 200)
   .attr("y", -60)
   .attr("font-size", "20px")
   .attr("text-anchor", "middle")
@@ -50,27 +51,22 @@ d3.csv("data/revenues.csv").then(data => {
     d.revenue = Number(d.revenue)
     d.profit = Number(d.profit)
   })
+
   d3.interval(() => {
     flag = !flag
     
     update(data)
-  }, 1000)
+  }, 1500)
 
- 
-})
   
+})
+
 function update(data) {
   const value = flag ? "profit" : "revenue"
-  const t = d3.transition().duration(1500)
+  const t = d3.transition().duration(1000)
 
   x.domain(data.map(d => d.month))
-  y.domain([0, d3.max(data, function(d){
-    if (value === "profit"){
-      return 9000
-    }else{
-      return 50
-    }
-  })])
+  y.domain([0, d3.max(data, d => d[value])])
 
   const xAxisCall = d3.axisBottom(x)
   xAxisGroup.transition(t).call(xAxisCall)
@@ -81,7 +77,7 @@ function update(data) {
       .attr("transform", "rotate(-40)")
 
   const yAxisCall = d3.axisLeft(y)
-    .ticks(3)
+    .ticks(5)
     .tickFormat(d => d)
   yAxisGroup.transition(t).call(yAxisCall)
 
@@ -108,13 +104,8 @@ function update(data) {
       .attr("x", (d) => x(d.month))
       .attr("width", x.bandwidth)
       .attr("y", d => y(d[value]))
-      .attr("height", d => (HEIGHT-150 - y(d[value])))
+      .attr("height", d => HEIGHT -150- y(d[value]))
 
-     
-
-  const text = flag ? "Profit ($)" : "Revenue ($)"
+  const text = flag ? "Average Price($)" : "Times Appeared on the Menu"
   yLabel.text(text)
-
-  
-   
 }
