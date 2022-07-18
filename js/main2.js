7faaaced41b77f35d3dd7430459b18b7d17d0836
MINJIE FU <melody.minjie.fu@gmail.com>
2022-07-18 01:26:23 -0600

    Add files via upload

--- js/main2.js
+++ js/main2.js
@@ -8,9 +8,9 @@ const svg = d3.select("#chart-area").append("svg")
   .attr("width", WIDTH + MARGIN.LEFT + MARGIN.RIGHT)
   .attr("height", HEIGHT + MARGIN.TOP + MARGIN.BOTTOM)
 
-  const g = svg.append("g")
+const g = svg.append("g")
   .attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
-  
+
 // Title
 svg.append("text")
    .attr("x", WIDTH / 2 + 130)
@@ -22,9 +22,9 @@ svg.append("text")
 
 
 // Y label
-g.append("text")
+const yLabel = g.append("text")
   .attr("class", "y axis-label")
-  .attr("x", - ((HEIGHT-150) / 2))
+  .attr("x", - (HEIGHT / 2))
   .attr("y", -60)
   .attr("font-size", "20px")
   .attr("text-anchor", "middle")
@@ -38,19 +38,18 @@ const x = d3.scaleBand()
 const y = d3.scaleLinear()
   .range([HEIGHT-150, 0])
 
-  const xAxisGroup = g.append("g")
+const xAxisGroup = g.append("g")
   .attr("class", "x axis")
   .attr("transform", `translate(0, 450)`)
 
 const yAxisGroup = g.append("g")
   .attr("class", "y axis")
 
-d3.csv("data/dishes.csv").then(data => {
+d3.csv("data/revenues.csv").then(data => {
   data.forEach(d => {
-    d.times = Number(d.times)
-    d.price = Number(d.price)
+    d.revenue = Number(d.revenue)
+    d.profit = Number(d.profit)
   })
-
   d3.interval(() => {
     flag = !flag
     const newData = flag ? data : data.slice(1)
@@ -59,18 +58,16 @@ d3.csv("data/dishes.csv").then(data => {
 
   update(data)
 })
-
+  
 function update(data) {
-  const value = flag ? "times" : "price"
-  const t = d3.transition().duration(750)
+  const value = flag ? "profit" : "revenue"
+  const t = d3.transition().duration(1500)
 
-  x.domain(data.map(d => d.name))
+  x.domain(data.map(d => d.month))
   y.domain([0, d3.max(data, d => d[value])])
 
-
-
   const xAxisCall = d3.axisBottom(x)
-  xAxisGroup.transition().call(xAxisCall)
+  xAxisGroup.transition(t).call(xAxisCall)
     .selectAll("text")
       .attr("y", "10")
       .attr("x", "-5")
@@ -80,11 +77,11 @@ function update(data) {
   const yAxisCall = d3.axisLeft(y)
     .ticks(10)
     .tickFormat(d => d)
-    yAxisGroup.transition().call(yAxisCall)
+  yAxisGroup.transition(t).call(yAxisCall)
 
   // JOIN new data with old elements.
   const rects = g.selectAll("rect")
-    .data(data, d => d.name)
+    .data(data, d => d.month)
 
   // EXIT old elements not present in new data.
   rects.exit()
@@ -94,22 +91,34 @@ function update(data) {
       .attr("y", y(0))
       .remove()
 
-    rects.enter().append("rect")
-      .attr("fill", "grey")
-      .attr("y", d => y(0))
-      .attr("x", (d) => x(d.name))
-      .attr("width", x.bandwidth)
-    .attr("height", d => (HEIGHT - 150 - y(0)))
-      .merge(rects)
-    .transition()
-    .duration(750)
-      .attr("x", (d) => x(d.name))
+  // ENTER new elements present in new data...
+  rects.enter().append("rect")
+    .attr("fill", "grey")
+    .attr("y", y(0))
+    .attr("height", 0)
+    // AND UPDATE old elements present in new data.
+    .merge(rects)
+    .transition(t)
+      .attr("x", (d) => x(d.month))
       .attr("width", x.bandwidth)
       .attr("y", d => y(d[value]))
-      .attr("height", d => HEIGHT-150 - y(d[value]))
-  
-      const text = flag ? "Times_Appeared" : "Average_Price ($)"
-      yLabel.text(text)
-    
+      .attr("height", d => (HEIGHT-150 - y(d[value])))
 
-  }
+      rects.enter().append("text")
+      .attr("x", (d) => x(d.month)+x.bandwidth()/2-11)
+      .attr("y", y(0))
+      .attr("height", 0)
+      // AND UPDATE old elements present in new data.
+      .merge(rects)
+      .transition(t)
+      .text( d => dd[value])
+      .attr("y",  d => yd[value]+0.1 )
+      .attr("dy", "-.7em"); 
+
+  const text = flag ? "Profit ($)" : "Revenue ($)"
+  yLabel.text(text)
+
+  rects.enter().append("text")
+    
+   
+}
